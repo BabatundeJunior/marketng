eaa521906c4076a20f429f4317f6d98e68ac12f8
BabatundeJunior <jbabs128@gmail.com>
2024-11-02 12:46:19 +0100

    updated files

--- marketng/settings.py
+++ marketng/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 load_dotenv()
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,6 +86,8 @@ DATABASES = {
     }
 }
 
+
+DATABASES['default'] = dj_database_url.parse(os.getenv('DATABASE_URL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
